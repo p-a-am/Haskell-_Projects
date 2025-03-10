6ffbc23e260b439cc00ee9a62e8b1214269fba4e
p-a-am <137822451+p-a-am@users.noreply.github.com>
2025-03-10 15:49:59 -0300

    Add files via upload

---
+++ Haskell/new 1.hs
@@ -0,0 +1,23 @@
+one_2_another::Int -> Int -> [Int]
+one_2_another x y = [x..y]
+
+int_to_list :: Int -> [Int]
+int_to_list x = [x]
+
+get_zeroes_out:: [Int] -> [Int]
+get_zeroes_out [0] = []
+get_zeroes_out x = x
+
+
+subtract_int_list :: [Int] -> [Int] -> Int -> [Int]
+subtract_int_list x y z= repeat_list((get_zeroes_out(int_to_list((y!!(z-1)) - (x!!0))))) (z-1)
+ where
+  repeat_list:: [Int] -> Int -> [Int]
+  repeat_list k s = (++) (repeat_list k s) (repeat_list k (s - 1))
+  z = length(y) - n
+  n = z
+  w::[Int]
+  w = []
+
+
+
