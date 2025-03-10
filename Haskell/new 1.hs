f9f918129e51a83e96b8fee4abb45bd2540b9cfe
p-a-am <137822451+p-a-am@users.noreply.github.com>
2025-03-10 19:20:36 -0300

    Update new 1.hs

--- Haskell/new 1.hs
+++ Haskell/new 1.hs
@@ -8,15 +8,15 @@ get_zeroes_out:: [Int] -> [Int]
 get_zeroes_out [0] = []
 get_zeroes_out x = x
 
-repeat_list:: [Int] -> (Int -> Int) -> [Int]
-repeat_list k (s - 0) = (++) (repeat_list k s) (repeat_list k (s - 1))
-repeat_list k (s - n) = return (s+1)
- where
-  n= (length(s) + 1)
-  
+repeat_list:: [Int] -> Int -> [Int]
+repeat_list k s
+  |length(k) < s = (++) (k) (repeat_list k (s - 1))
+  |otherwise = return(k!!0)
+--repeat_list k (-1) = return 0
+
 
 subtract_int_list :: [Int] -> [Int] -> [Int]
-subtract_int_list x y= repeat_list((get_zeroes_out(int_to_list((y!!(z-1)) - (x!!0))))) (z-1)
+subtract_int_list x y= (repeat_list((get_zeroes_out(int_to_list((y!!(z-1)) - (x!!0))))) (z-1))
  where
   z = length(y)
 
